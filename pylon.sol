fa95e6d8fd31f0cf870be2eb4654c857b54e4a3b
Delphi Systems <contact@delphi.systems>
2018-03-20 17:47:01 +0900

    Implement setters, getters, and register statuses

--- pylon.sol
+++ pylon.sol
@@ -14,16 +14,28 @@ pragma solidity ^0.4.15;
 \***************************************************************************/
 
 contract Pylon {
+    /******************\
+     *  Value Status  *
+     ********************************************************************\
+     *  @dev The different possible value statuses:
+     *       Active: value is not yet set (predictions can be made)
+     *       Locked: value is not yet set (predictions cannot be made)
+     *       Finalized: value is set
+    \********************************************************************/
+    // enum State { Active, Locked, Finalized }
+
     /*************\
      *  Storage  *
     \*************/
-    mapping(address => mapping(bytes32 => bytes32)) public values;      // Oracle address => register => value
+    mapping(address => mapping(address => bytes32)) public values;      // Oracle address => register => value
+    mapping(address => mapping(address => uint)) public statuses;       // Oracle address => register => status of register
     bool public rewritable;                                             // Whether values can be updated after initial write
 
     /************\
      *  Events  *
     \************/
-    event OracleOutput(address oracle, bytes32 register, bytes32 value);
+    event OracleOutput(address oracle, address register, bytes32 value);
+    event StatusUpdate(address oracle, address register, uint status);
 
     /*********************\
      *  Public functions
@@ -38,14 +50,62 @@ contract Pylon {
      *  @dev Set function
      *  @param register Output register written to
      *  @param value Value written to register
-     *  @return Whether write was successful
     \***********************************************/
-    function set(bytes32 register, bytes32 value) public returns (bool success) {
-        if (values[msg.sender][register].length > 0 && !rewritable) {
-            return false;
-        }
+    function set(address register, bytes32 value) public {
+        require(statuses[msg.sender][register] < 2 || rewritable);
 
         values[msg.sender][register] = value;
         OracleOutput(msg.sender, register, value);
+
+        // After value is set, update status (2 == State.Finalized)
+        setStatus(register, 2);
+    }
+
+    /***************************************************\
+     *  @dev Get function
+     *  @param register Oracle lookup retrieved from
+     *  @param register Value register retrieved from
+     *  @return Value from register
+    \***************************************************/
+    function get(address oracle, address register) public constant returns (bytes32 value) {
+        return values[oracle][register];
+    }
+
+    /************************************************\
+     *  @dev setStatus function
+     *  @param register Relevant register to update
+     *  @param status Status to set register to
+     *  Will not allow status to drop unless
+     *    the pylon is rewritable
+    \************************************************/
+    function setStatus(address register, uint status) public {
+        // For now, there's no status > Finalized
+        require(status <= 2);
+        require(rewritable || statuses[msg.sender][register] < status);
+        statuses[msg.sender][register] = status;
+        StatusUpdate(msg.sender, register, status);
+    }
+
+    /***************************************************\
+     *  @dev incrementStatus function
+     *  @param register Relevant register to increment
+    \***************************************************/
+    function incrementStatus(address register) public {
+        uint status = statuses[msg.sender][register] + 1;
+        // For now, there's no status > Finalized
+        require(status <= 2);
+
+        statuses[msg.sender][register] = status;
+        StatusUpdate(msg.sender, register, status);
+    }
+
+    /***************************************************\
+     *  @dev getStatus function
+     *  @param register Oracle lookup retrieved from
+     *  @param register Status register retrieved from
+     *  @return Status from register
+    \***************************************************/
+    function getStatus(address oracle, address register) public constant returns (uint status) {
+        return statuses[oracle][register];
     }
 }
